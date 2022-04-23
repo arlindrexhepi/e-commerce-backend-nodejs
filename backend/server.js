4c39e3310dbf5f4eca9bfc00885edc5421618429
arlindrexhepi <axr3649@g.rit.edu>
2022-04-23 02:32:09 +0200

    created apis

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-console.log('hello world');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const app = express();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/products', require('./routes/productRoutes'));
+app.use('/api/categories', require('./routes/categoryRoutes'));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
